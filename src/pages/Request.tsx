0b346671971cbe2428e57c593e677a52ccad8da1
Regashii <130679673+Regashii@users.noreply.github.com>
2023-04-14 00:24:30 +0800

    add feature in request

--- src/pages/Request.tsx
+++ src/pages/Request.tsx
@@ -1,7 +1,26 @@
 const Request = () => {
+  const orders = [
+    "order1",
+    "order2",
+    "order3",
+    "order4",
+    "order5",
+    "order6",
+    "order7",
+    "order8",
+    "order9",
+    "order10",
+  ];
   return (
     <div className="Pages1">
-      <h1>Request</h1>
+      <header>
+        <h1>Request</h1>
+      </header>
+      <body>
+        {orders.map((order) => (
+          <div className="order">{order}</div>
+        ))}
+      </body>
     </div>
   );
 };
