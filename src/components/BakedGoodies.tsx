0fdb559d8d574d23655c6ba2787d90644e9ceada
Regashii <130679673+Regashii@users.noreply.github.com>
2023-04-14 22:32:37 +0800

    new add

--- src/components/BakedGoodies.tsx
+++ src/components/BakedGoodies.tsx
@@ -14,10 +14,37 @@ import Date from "../pages/Date";
 const BakedGoodies = () => {
   let changeStyle = { width: "3m", height: "2em" };
   const [page, setPage] = useState("request");
+  const [tab, setTab] = useState("0");
+
+  function sideClick() {
+    setTab("0");
+  }
+
+  function close() {
+    if (tab === "13.3em") {
+      setTab("0");
+    }
+  }
 
   return (
     <>
-      <div className="Home">
+      <div className="Home" onClick={close}>
+        <div className="dashboard">
+          <div
+            className="dashbox1"
+            onClick={() => {
+              setTab("13.3em");
+            }}
+          >
+            <p>Open</p>
+          </div>
+          <div className="dashbox2">
+            <h1>Dashboard</h1>
+          </div>
+          <div className="dashbox3">
+            <img src="BakedGoodies.png" alt="Logo" />
+          </div>
+        </div>
         {page === "request" && <Request />}
 
         {page === "onGoing" && <Ongoing />}
@@ -25,41 +52,54 @@ const BakedGoodies = () => {
         {page === "history" && <History />}
 
         {page === "date" && <Date />}
-
-        <div className="downBar">
+        <div className="sideBar" style={{ width: tab }}>
           <div
-            className="icon"
+            className="con1"
             onClick={() => {
               setPage("request");
+              sideClick();
             }}
           >
-            <FontAwesomeIcon icon={faEnvelope} style={changeStyle} />
+            <div className="icon">
+              <FontAwesomeIcon icon={faEnvelope} style={changeStyle} />
+            </div>
+            <h4>Request</h4>
           </div>
-
           <div
-            className="icon"
+            className="con1"
             onClick={() => {
               setPage("onGoing");
+              sideClick();
             }}
           >
-            <FontAwesomeIcon icon={faCakeCandles} style={changeStyle} />
+            <div className="icon">
+              <FontAwesomeIcon icon={faCakeCandles} style={changeStyle} />
+            </div>
+            <h4>Ongoing</h4>
           </div>
           <div
-            className="icon"
+            className="con1"
             onClick={() => {
               setPage("history");
+              sideClick();
             }}
           >
-            <FontAwesomeIcon icon={faClockRotateLeft} style={changeStyle} />
+            <div className="icon">
+              <FontAwesomeIcon icon={faClockRotateLeft} style={changeStyle} />
+            </div>
+            <h4>History</h4>
           </div>
-
           <div
-            className="icon"
+            className="con1"
             onClick={() => {
               setPage("date");
+              sideClick();
             }}
           >
-            <FontAwesomeIcon icon={faCalendar} style={changeStyle} />
+            <div className="icon">
+              <FontAwesomeIcon icon={faCalendar} style={changeStyle} />
+            </div>
+            <h4>Date</h4>
           </div>
         </div>
       </div>
