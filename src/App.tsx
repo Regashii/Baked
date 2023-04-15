90ac3d31daff34e1689aee9dd00c76c086d84b09
Regashii <130679673+Regashii@users.noreply.github.com>
2023-04-16 01:18:18 +0800

    new css

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
 import "./App.css";
+import "./Sign.css";
 import { useState } from "react";
 import BakedGoodies from "./components/BakedGoodies";
 
 function App() {
-  const [wrong, setWrong] = useState("");
+  const [check1, setCheck1] = useState(false);
+  const [check2, setCheck2] = useState(false);
+  const [check3, setCheck3] = useState(false);
+
   const [signin, setSignin] = useState(true);
   const [goodies, setSGoodies] = useState(false);
 
   const userName = "Bakedgoodies";
   const passWord = "12345";
 
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [login, setLogin] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setLogin((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
 
   function signClick() {
-    if (username === userName && password === passWord) {
-      setSignin(false);
-      setSGoodies(true);
+    if (login.username === "") {
+      setCheck1(true);
+      setCheck3(false);
     } else {
-      console.log("Incorrect");
-      setWrong("Incorrect");
+      setCheck1(false);
+    }
+
+    if (login.password === "") {
+      setCheck2(true);
+      setCheck3(false);
+    } else {
+      setCheck2(false);
+    }
+
+    if (login.username != "" && login.password != "") {
+      if (login.username === userName && login.password === passWord) {
+        setSignin(false);
+        setSGoodies(true);
+      } else {
+        setCheck3(true);
+      }
     }
   }
 
@@ -27,26 +55,34 @@ function App() {
     <>
       {signin && (
         <div className="sign-in">
-          <div className="box">
-            <h2>Sign in</h2>
-            <input
-              type="text"
-              placeholder="Username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <br />
-            <input
-              type="text"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <br />
-            <span>{wrong}</span>
-            <br />
+          <div className="card">
+            <a className="login">Log in</a>
+            <div className="inputBox">
+              <input
+                type="text"
+                required
+                name="username"
+                onChange={handleChange}
+              />
+              <span className="user">Username</span>
+              {check1 && <p>Please input your usernames</p>}
+            </div>
 
-            <button onClick={signClick}>Bake</button>
+            <div className="inputBox">
+              <input
+                type="password"
+                required
+                name="password"
+                onChange={handleChange}
+              />
+              <span>Password</span>
+              {check2 && <p>Please input your password</p>}
+            </div>
+            {check3 && <i>Incorrect username or password</i>}
+
+            <button className="enter" onClick={signClick}>
+              Enter
+            </button>
           </div>
         </div>
       )}
