b0bf12f4f7687a67b99f33dfa39af7608f83acdf
Regashii <130679673+Regashii@users.noreply.github.com>
2023-04-19 14:11:26 +0800

    delete some

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import "./Sign.css";
-import "./pagescss/Admin.css";
+
 import "./pagescss/Request.css";
 import "./pagescss/History.css";
 import "./pagescss/Ongoing.css";
@@ -9,7 +9,6 @@ import "./pagescss/Date.css";
 import Login from "./components/Login";
 import "./pagescss/Request.css";
 import ChangePass from "./components/ChangePass";
-import Admin from "./components/Admin";
 
 function App() {
   return (
